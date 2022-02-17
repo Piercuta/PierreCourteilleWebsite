83b3c5f79ace4b1ae0588bd7321acd8425e24446
Piercuta <pierre.courteille@gmail.com>
2022-02-17 18:58:32 +0100

    update website category

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -10,26 +10,34 @@
 //       });
 //   });
 
-  $(document).ready(function() {
-    $(window).scroll( function(){
-        $('.fade-in-down').each( function(i){
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            if( bottom_of_window > bottom_of_element ){
-              $(this).show();
-              $(this).addClass( "fade-in-down-start" );
-            }
-        });
+var count = 0;
 
-        $('.fade-in-up').each( function(i){
-          var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-          var bottom_of_window = $(window).scrollTop() + $(window).height();
-          if( bottom_of_window > bottom_of_element ){
-            $(this).show();
-            $(this).addClass( "fade-in-up-start" );
-          }
-      });
-    });
+
+function fade(){
+  $('.fade-in-down').each( function(i){
+      var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      if( bottom_of_window > bottom_of_element ){
+        $(this).show();
+        $(this).addClass( "fade-in-down-start" );
+        count = count +1;
+        console.log(count);
+      }
+  });
+
+  $('.fade-in-up').each( function(i){
+    var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+    if( bottom_of_window > bottom_of_element ){
+      $(this).show();
+      $(this).addClass( "fade-in-up-start" );
+    }
+  });
+  $(window).unbind("scroll");
+}
+
+$(document).ready(function() {
+  $(window).scroll(fade);
 });
 
 
